a6beea18681b5bd24f28176b5509b5d7ee6a520e
Sara Webb <saracharlotterose@gmail.com>
2020-08-01 12:29:09 -0700

    Updating models

---
+++ models/workout.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    
+  },
+
+  exercises: {
+    type: []
+    
+    },
+  
+
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
+
