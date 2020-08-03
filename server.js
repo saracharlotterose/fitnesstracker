23c97777c46f1169cfa8a8d6341ddf2d85bce2f7
Sara Webb <saracharlotterose@gmail.com>
2020-08-02 19:05:44 -0700

    Adding create function for workout

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 // morgan logs our database
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const db = require("./models");
 
@@ -19,20 +19,27 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
 });
+// html routes
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname, "/public/index.html");
+  });
 app.get("/exercise", (req, res) => {
   res.sendfile(__dirname + "/public/exercise.html");
 });
 
+
 app.get("/stats", (req, res) => {
     res.sendfile(__dirname + "/public/stats.html");
    
-    
+
   
 })
+
+
+  //api routes
 app.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
-    .populate("exercises")
     .then(dbWorkouts => {
         console.log(dbWorkouts)
         res.json(dbWorkouts);
@@ -41,15 +48,36 @@ app.get("/api/workouts/range", (req, res) => {
         res.json(err);
       });
 })
-app.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.json(err);
-      });
-  });
+
+
+
+
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body, (err, data)=>{
+if (err){
+    console.log(err);
+  
+}else{
+    res.json(data);
+}
+    })
+    
+});
+
+
+
+
+
+
+
+
+   
+
+  
+
+
+
+
 
 
 
