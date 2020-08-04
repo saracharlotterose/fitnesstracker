631d645a206dd049cb87465a4ee3149cfacb60c8
Sara Webb <saracharlotterose@gmail.com>
2020-08-03 18:02:03 -0700

    adding dynamic URI for deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 4000;
-
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 const db = require("./models");
 
 
